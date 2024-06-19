ce8055f27caf05c1a4d026ec69c552f7fa962cf0
Olivier <olivierdijkstra@gmail.com>
2024-06-19 18:24:36 +0200

    redo 404 page

--- apps/dashboard/src/app/dashboard/not-found.tsx
+++ apps/dashboard/src/app/dashboard/not-found.tsx
@@ -4,17 +4,18 @@ import { Button } from '@/components/ui/button';
 
 export default async function Page() {
   return (
-    <div className='mx-auto flex max-w-md flex-col gap-2'>
-      <h1 className='text-4xl font-semibold'>Page Not Found</h1>
+    <div className='mx-auto mt-12 flex max-w-md flex-col items-center justify-center gap-2 text-center'>
+      <h1 className='text-8xl font-bold'>404</h1>
 
       <p className='text-muted-foreground'>
-        The page you are looking for does not exist. Please check the URL in the
-        address bar and try again.
+        Oops, the page you are looking for does not exist.
       </p>
 
-      <Link href='/dashboard'>
-        <Button>Go to Dashboard</Button>
-      </Link>
+      <div className='flex justify-center mt-4'>
+        <Link href='/dashboard'>
+          <Button>Go to Dashboard</Button>
+        </Link>
+      </div>
     </div>
   );
 }
