7c6921d3502a68855013dd0ac634a33d0572a574
Olivier <olivierdijkstra@gmail.com>
2024-06-23 23:34:12 +0200

    Add payment refund route to API

--- apps/api/routes/api.php
+++ apps/api/routes/api.php
@@ -74,6 +74,7 @@ Route::get('orders/{order:order_number}/is-paid', [OrderController::class, 'isPa
 // Payments
 //
 Route::get('orders/{order}/payments', [PaymentsController::class, 'index'])->name('orders.payments.index');
+Route::post('payments/{payment}/refund', [PaymentsController::class, 'refund'])->name('payments.refund');
 
 //
 // Stats
