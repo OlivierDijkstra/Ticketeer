72005efc5794ce3979c0438cf8e62d882764f527
Olivier <olivierdijkstra@gmail.com>
2024-06-24 00:12:48 +0200

    Add RefundPaymentRequest form request class

---
+++ apps/api/app/Http/Requests/RefundPaymentRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RefundPaymentRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'amount' => ['required', 'numeric', 'min:0', 'max:' . $this->payment->amount - $this->payment->amount_refunded],
+        ];
+    }
+}
