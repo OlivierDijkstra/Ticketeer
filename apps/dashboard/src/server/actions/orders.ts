3875afd1df108733cc0dcaf2427bd7a80553296b
Olivier <olivierdijkstra@gmail.com>
2024-06-27 17:28:18 +0200

    Update order action to allow updating customer ID and description

--- apps/dashboard/src/server/actions/orders.ts
+++ apps/dashboard/src/server/actions/orders.ts
@@ -100,7 +100,8 @@ export async function updateOrderAction({
 }: {
   order_id: string;
   data: {
-    description: string | null;
+    customer_id?: string | null;
+    description?: string | null;
   };
 }) {
   return await fetchWithAuth<Order>(`api/orders/${order_id}`, {
