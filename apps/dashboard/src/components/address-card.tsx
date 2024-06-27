dbce335352256f9867f9fbb4e58d9a8ec32757c6
Olivier <olivierdijkstra@gmail.com>
2024-06-27 17:28:38 +0200

    Refactor address display for street and street2 fields

--- apps/dashboard/src/components/address-card.tsx
+++ apps/dashboard/src/components/address-card.tsx
@@ -23,7 +23,7 @@ export default function AddressCard({ address }: { address?: Address }) {
         <ul className='grid gap-3'>
           <li className='flex items-center justify-between text-right'>
             <span className='text-muted-foreground'>Street</span>
-            <span>{`${address?.street}, ${address?.street2 || ''}`}</span>
+            <span>{`${address?.street || ''}${address?.street2 ? `, ${address?.street2}` : ''}`}</span>
           </li>
 
           <li className='flex items-center justify-between'>
