86398e87633424a27e13dd42434030b835965a79
Olivier <olivierdijkstra@gmail.com>
2024-06-27 17:30:11 +0200

    Add PostCSS and TailwindCSS dependencies

---
+++ apps/api/resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>@yield('title')</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
