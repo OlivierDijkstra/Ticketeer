4e9aa309dfdce5f0aa035a26fc7dc832556d7054
Olivier <olivierdijkstra@gmail.com>
2024-06-28 01:09:18 +0200

    Add notification controller and request for ticket notifications

---
+++ apps/api/app/Http/Controllers/NotificationController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TicketsNotificationRequest;
+use App\Models\Order;
+use App\Notifications\TicketsNotification;
+
+class NotificationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function notifyTickets(TicketsNotificationRequest $request)
+    {
+        $order = Order::find($request->order_id);
+        
+        if (!$order->customer) {
+            return response()->json(['message' => 'Customer not found'], 404);
+        }
+
+        $order->customer->notify(new TicketsNotification($order));
+
+        return response()->json(['message' => 'Tickets notification sent'], 200);
+    }
+}
