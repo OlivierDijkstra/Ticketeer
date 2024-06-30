8450538027c06d7a7279ec76e736d2024b7b0242
Olivier <olivierdijkstra@gmail.com>
2024-06-30 02:25:53 +0200

    Update createUrl and fetchJson calls in page.tsx

--- apps/frontend/src/app/event/[event]/[show]/page.tsx
+++ apps/frontend/src/app/event/[event]/[show]/page.tsx
@@ -1,4 +1,4 @@
-import { type Event, type Show } from '@repo/lib';
+import { createUrl, type Event, type Show } from '@repo/lib';
 
 import CreateOrderForm from '@/components/forms/create-order-form';
 import { fetchJson } from '@/lib/fetch';
@@ -8,9 +8,15 @@ export default async function Page({
 }: {
   params: { event: string; show: string };
 }) {
-  const event = await fetchJson<Event>(`/api/events/${params.event}`);
+  const event = await fetchJson<Event>(
+    `/api/events/${params.event}?enabled=true`
+  );
 
-  const show = await fetchJson<Show>(`/api/shows/${params.show}`);
+  const showUrl = createUrl(`/api/shows/${params.show}`, {
+    enabled: true,
+  });
+
+  const show = await fetchJson<Show>(showUrl);
 
   return (
     <div className='grid grid-cols-1 gap-4 lg:grid-cols-3'>
@@ -20,7 +26,10 @@ export default async function Page({
       </div>
 
       <div className='lg:col-span-2'>
-        <CreateOrderForm products={show.products} show={show} />
+        <CreateOrderForm
+          products={show.products.filter((product) => product.pivot?.enabled)}
+          show={show}
+        />
       </div>
     </div>
   );
