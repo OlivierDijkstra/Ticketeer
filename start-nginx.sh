ed453810e5fc113ffe53ebc6adc9f4d25fef016f
Olivier <olivierdijkstra@gmail.com>
2024-07-02 19:14:52 +0200

    test

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-envsubst '\$DOMAIN \$API_DOMAIN' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '\$DOMAIN \$API_DOMAIN \$API_PORT' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/nginx.conf
 nginx -g 'daemon off;'
