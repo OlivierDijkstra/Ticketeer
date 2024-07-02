a92d885d37597071269223a6b71ab65e19bde201
Olivier <olivierdijkstra@gmail.com>
2024-07-03 01:20:04 +0200

    test

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,24 +1,25 @@
 export const dynamic = 'force-dynamic';
 
-import { createUrl, type Event } from '@repo/lib';
+// import { createUrl, type Event } from '@repo/lib';
 
-import EventCard from '@/components/event-card';
-import { fetchJson } from '@/lib/fetch';
+// import EventCard from '@/components/event-card';
+// import { fetchJson } from '@/lib/fetch';
 
 export default async function Page() {
-  const eventsUrl = createUrl('/api/events', {
-    enabled: true,
-  });
+  // const eventsUrl = createUrl('/api/events', {
+  //   enabled: true,
+  // });
 
-  const events = await fetchJson<Event[]>(eventsUrl, {
-    cache: 'no-cache',
-  });
+  // const events = await fetchJson<Event[]>(eventsUrl, {
+  //   cache: 'no-cache',
+  // });
 
   return (
-    <main className='container'>
-      {events.map((event) => (
-        <EventCard key={event.id} event={event} />
-      ))}
-    </main>
+    <p>Test</p>
+    // <main className='container'>
+    //   {events.map((event) => (
+    //     <EventCard key={event.id} event={event} />
+    //   ))}
+    // </main>
   );
 }
