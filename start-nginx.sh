dfb1410520a14db75642acf180d088c2ddc69a8e
Olivier <olivierdijkstra@gmail.com>
2024-07-03 00:31:16 +0200

    Update Dockerfile to expose port 8001 and adjust ports

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-envsubst '\$DOMAIN \$API_DOMAIN \$API_PORT' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '\$FRONTEND_DOMAIN \$DASHBOARD_DOMAIN \$API_DOMAIN \$API_PORT' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/nginx.conf
 nginx -g 'daemon off;'
