9b0c57fe7cea4e64fa488854af4d89e3a68189af
Olivier <olivierdijkstra@gmail.com>
2024-08-07 15:34:55 +0200

    Update default currency to USD in app config

--- apps/api/config/app.php
+++ apps/api/config/app.php
@@ -84,7 +84,7 @@ return [
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
 
-    'currency' => env('APP_CURRENCY', 'EUR'),
+    'currency' => env('APP_CURRENCY', 'USD'),
 
     /*
     |--------------------------------------------------------------------------
