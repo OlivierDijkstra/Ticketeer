778e85703bf0563dc6c8acfd7a7aa33890d650ef
Olivier <olivierdijkstra@gmail.com>
2024-08-11 19:25:38 +0200

    Update charts with suspense and skeleton loading

--- apps/dashboard/src/app/dashboard/page.tsx
+++ apps/dashboard/src/app/dashboard/page.tsx
@@ -1,7 +1,9 @@
 import { Suspense } from 'react';
 
+import ChartWrapper from '@/components/charts/chart-wrapper';
 import CustomersChart from '@/components/charts/customers-chart';
 import RevenueChart from '@/components/charts/revenue-chart';
+import SkeletonChart from '@/components/skeletons/skeleton-chart';
 import SkeletonStatistic from '@/components/skeletons/skeleton-statistic';
 import SkeletonTable from '@/components/skeletons/skeleton-table';
 import NewOrdersStatistic from '@/components/statistics/new-orders-statistic';
@@ -17,18 +19,38 @@ export default async function Page({
 }) {
   return (
     <div className='space-y-4'>
-      <div className='grid grid-cols-1 gap-4 sm:grid-cols-3'>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-3'>
         <Suspense fallback={<SkeletonStatistic />}>
-          <NewOrdersStatistic />
+          <NewOrdersStatistic className='sm:max-w-none' />
         </Suspense>
+
         <Suspense fallback={<SkeletonStatistic />}>
-          <RevenueStatistic />
+          <RevenueStatistic className='sm:max-w-none' />
         </Suspense>
       </div>
 
       <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
-        <RevenueChart />
-        <CustomersChart />
+        <Suspense fallback={<SkeletonChart />}>
+          <ChartWrapper
+            Chart={RevenueChart}
+            modelType='Order'
+            defaultDateRange='This year'
+            defaultGranularity='month'
+            aggregationType='sum'
+            queryKey='revenueChart'
+          />
+        </Suspense>
+
+        <Suspense fallback={<SkeletonChart />}>
+          <ChartWrapper
+            Chart={CustomersChart}
+            modelType='Customer'
+            aggregationType='count'
+            defaultDateRange='This month'
+            defaultGranularity='day'
+            queryKey='customersChart'
+          />
+        </Suspense>
       </div>
 
       <Suspense fallback={<SkeletonTable />}>
