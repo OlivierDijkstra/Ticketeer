3f1e8a6e398cab6aa5c88bb09b36b250a8cca4ee
Olivier <olivierdijkstra@gmail.com>
2024-08-14 20:29:48 +0200

    Add GenerateMonthlyReportJob to monthly schedule

--- apps/api/routes/console.php
+++ apps/api/routes/console.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Jobs\AggregateDataJob;
+use App\Jobs\GenerateMonthlyReportJob;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::job(new AggregateDataJob('hour'))->hourly(0);
@@ -8,5 +9,7 @@ Schedule::job(new AggregateDataJob('day'))->dailyAt('00:00');
 Schedule::job(new AggregateDataJob('week'))->weekly();
 Schedule::job(new AggregateDataJob('month'))->monthly();
 Schedule::job(new AggregateDataJob('year'))->yearly();
+
+Schedule::job(new GenerateMonthlyReportJob())->monthly();
 
 Schedule::command('app:clean-tmp-directory')->daily();
