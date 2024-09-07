62a9d17e2d7988f3f5a23364e9526b5a0739052d
Olivier <olivierdijkstra@gmail.com>
2024-09-07 18:14:41 +0200

    fix guestlist generation

--- apps/api/app/Jobs/GenerateGuestListPdfJob.php
+++ apps/api/app/Jobs/GenerateGuestListPdfJob.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
 use Spatie\Browsershot\Browsershot;
 use Spatie\LaravelPdf\Facades\Pdf;
 
@@ -24,7 +25,15 @@ class GenerateGuestListPdfJob implements ShouldQueue
 
     public function handle(): void
     {
-        $pdfPath = storage_path("app/guest-lists/{$this->show->id}-guest-list.pdf");
+        if (! Storage::exists('tmp')) {
+            Storage::makeDirectory('tmp');
+        }
+
+        $file_path = storage_path("app/tmp/{$this->show->id}-guest-list.pdf");
+
+        if (Storage::exists($file_path)) {
+            Storage::delete($file_path);
+        }
 
         $tickets = $this->show->tickets()
             ->with(['order.customer', 'product'])
@@ -41,8 +50,8 @@ class GenerateGuestListPdfJob implements ShouldQueue
                     ->showBackground();
             })
             ->format('a4')
-            ->save($pdfPath);
+            ->save($file_path);
 
-        $this->user->notify(new GuestListNotification($this->show, $pdfPath));
+        $this->user->notify(new GuestListNotification($this->show, $file_path));
     }
 }
